72300df19c35992bab9da6677ea3c3c7ce1ae6ea
Galin McMahon <galinmcmahon@gmail.com>
2017-05-05 11:42:54 -0700

    pass save spec and modify all method

--- lib/Projects.rb
+++ lib/Projects.rb
@@ -7,24 +7,25 @@ class Projects
     @id = attributes.fetch(:id)
   end
 
+  define_singleton_method(:all) do
+  returned_projects = DB.exec("SELECT * FROM projects;")
+  projects = []
+  returned_projects.each() do |project|
+    name_project = project.fetch("name_project")
+    id = project.fetch("id").to_i()
+    projects.push(Projects.new({:id => id, :name_project => name_project}))
+  end
+  projects
+  end
+
   define_method(:==) do |other|
     same_class = self.class().eql?(other.class())
     same_name_project = self.name_project().eql?(other.name_project())
     same_class.&(same_name_project)
   end
 
-  define_singleton_method(:all) do
-  returned_projects = DB.exec("SELECT * FROM projects;")
-  projects = []
-  returned_projects.each() do |task|
-    description = task.fetch("name_project")
-    projects.push(Projects.new({:name_project => name_project}))
-  end
-  projects
-  end
-
   define_method(:save) do
-  result = DB.exec("INSERT INTO projects (name_project) VALUES ('#{@name}') RETURNING id;")
+  result = DB.exec("INSERT INTO projects (name_project) VALUES ('#{@name_project}') RETURNING id;")
   @id = result.first().fetch("id").to_i()
   end
 
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -17,28 +17,18 @@ describe(Projects) do
     end
   end
 
-  # describe('#save') do
-  #   it("creates a new project in the volunteer_tracker database") do
-  #     test_project = Projects.new()
-  #     test_project.save
-  #     found_DB_objects = DB.exec("SELECT * FROM projects WHERE name_project = 'Field Mowing';")
-  #     expect(found_DB_objects[0]["name_project"]).to(eq('Field Mowing'))
-  #   end
-  # end
-
-
-  # describe("#save") do
-  #   it("adds a task to the array of saved tasks") do
-  #     test_project = Projects.new({:name_project => "Field Mowing"})
-  #     test_project.save()
-  #     expect(Projects.all()).to(eq([test_project]))
-  #   end
-  # end
-
   describe("#name_project") do
     it("lets you see existing project") do
       test_project = Projects.new({:id => nil, :name_project => "Field Mowing"})
       expect(test_project.name_project()).to(eq("Field Mowing"))
+    end
+  end
+
+  describe("#save") do
+    it("adds a task to the array of saved tasks") do
+      test_project = Projects.new({:id => nil, :name_project => "Field Mowing"})
+      test_project.save()
+      expect(Projects.all()).to(eq([test_project]))
     end
   end
 
