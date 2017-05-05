e5c98324442489e7bb96a4a88bdbad7e433703bd
Galin McMahon <galinmcmahon@gmail.com>
2017-05-05 11:20:21 -0700

    pass name_project spec to allow user to view all projects

--- lib/Projects.rb
+++ lib/Projects.rb
@@ -1,9 +1,10 @@
 class Projects
 
-  attr_reader(:name_project)
+  attr_reader(:name_project, :id)
 
-  define_method(:initialize) do |name_project|
-    @name_project = name_project
+  define_method(:initialize) do |attributes|
+    @name_project = attributes.fetch(:name_project)
+    @id = attributes.fetch(:id)
   end
 
   define_method(:==) do |other|
@@ -20,6 +21,11 @@ class Projects
     projects.push(Projects.new({:name_project => name_project}))
   end
   projects
+  end
+
+  define_method(:save) do
+  result = DB.exec("INSERT INTO projects (name_project) VALUES ('#{@name}') RETURNING id;")
+  @id = result.first().fetch("id").to_i()
   end
 
 
--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -5,8 +5,8 @@ describe(Projects) do
 
   describe("#==") do
     it("is the same project if it has the same name") do
-      project1 = Projects.new({:description => "Tree Planting"})
-      project2 = Projects.new({:description => "Tree Planting"})
+      project1 = Projects.new({:name_project => "Tree Planting", :id => nil})
+      project2 = Projects.new({:name_project => "Tree Planting", :id => nil})
       expect(project1).to(eq(project2))
     end
   end
@@ -17,16 +17,6 @@ describe(Projects) do
     end
   end
 
-  # describe("#name_project") do
-  #   it("lets you see existing project") do
-  #     test_project = Projects.new({:name_project => "Field Mowing", :id => nil})
-  #     expect(test_project.name_project()).to(eq("Field Mowing"))
-  #   end
-  # end
-
-
-
-
   # describe('#save') do
   #   it("creates a new project in the volunteer_tracker database") do
   #     test_project = Projects.new()
@@ -35,4 +25,24 @@ describe(Projects) do
   #     expect(found_DB_objects[0]["name_project"]).to(eq('Field Mowing'))
   #   end
   # end
+
+
+  # describe("#save") do
+  #   it("adds a task to the array of saved tasks") do
+  #     test_project = Projects.new({:name_project => "Field Mowing"})
+  #     test_project.save()
+  #     expect(Projects.all()).to(eq([test_project]))
+  #   end
+  # end
+
+  describe("#name_project") do
+    it("lets you see existing project") do
+      test_project = Projects.new({:id => nil, :name_project => "Field Mowing"})
+      expect(test_project.name_project()).to(eq("Field Mowing"))
+    end
+  end
+
+
+
+
 end
